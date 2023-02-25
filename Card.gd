e229de74c61db185f62de399af1131b6f909364f
Arturo Miguel Lameg <2316415L@student.gla.ac.uk>
2023-02-25 22:01:36 +0000

    implement shapes logic

--- Card.gd
+++ Card.gd
@@ -2,7 +2,9 @@ class_name Card extends Node
 
 
 var Card = {}
+onready var Shapes = get_node("/root/Game/Shapes")
+
+# get the shape objects on a card!!!
 
 func _init():
 	pass
-
--- Shapes.gd
+++ Shapes.gd
@@ -1,55 +1,42 @@
 extends ShapeBehaviour
 
 var Shapes = {}
+var Cost = {}
+
+onready var Bomb = get_node('root/Game/Bomb')
 
 func _init():
-	Shapes[1] = [[Vector2(0,0),true],[Vector2(0,1),false],[Vector2(-1,1),true],[Vector2(0,2),false],[Vector2(1,2),true],[Vector2(0,3),true]]
-	Shapes[2]  = [[Vector2(0,0), true], [Vector2(0,1), false], [Vector2(0,2), false], [Vector2(1,2), true]]
-	Shapes[3] = [[Vector2(0,0),false],[Vector2(0,1),true],[Vector2(1,0),true]]
-	Shapes[4] = [[Vector2(0,0),true],[Vector2(0,1),false],[Vector2(-1,1),true],[Vector2(0,2),true],[Vector2(1,1),true]]
-	Shapes[5] = [[Vector2(0,0),true],[Vector2(0,1),true]]
-	
-	Shapes[6] = [] + Shapes[5]
-	Shapes[6][1] = [Vector2(0,1),false]
-	Shapes[6] = Shapes[6].append([[Vector2(0,2),false],[Vector2(0,3),true]])
-	
-	Shapes[7] = [] + Shapes[5]
-	Shapes[7] = Shapes[7].append([[Vector2(0,2),true],[Vector2(1,1),true]])
-	
-	Shapes[8] = [] + Shapes[7]
-	Shapes[8] = Shapes[8].append([[Vector2(0,3),true],[Vector2(1,2),true]])
-	
-	Shapes[9] = [[Vector2(0,0),true],[Vector2(0,1),false],[Vector2(1,1),false],[Vector2(1,2),true]]
-	
-	Shapes[10] = [[Vector2(0,0),true],[Vector2(0,1),false,'sink']]
-	Shapes[11]  = [[Vector2(0,0),false,'sink']]
-	
-	Shapes[12] = mirror_horizontal(Shapes[9])
-	Shapes[13] = mirror_horizontal(Shapes[2])
-	
-	Shapes[14] = mirror_horizontal(Shapes[1])
-	
-	Shapes[15] = [[Vector2(0,0),'connector']]
-	
-	Shapes[16] = get_node('root/Game/Bomb')
+	Shapes[0] = [[Vector2(0,0),true],[Vector2(0,1),false],[Vector2(-1,1),true],[Vector2(0,2),false],[Vector2(1,2),true],[Vector2(0,3),true]]
+	Shapes[1]  = [[Vector2(0,0), true], [Vector2(0,1), false], [Vector2(0,2), false], [Vector2(1,2), true]]
+	Shapes[2] = [[Vector2(0,0),false],[Vector2(0,1),true],[Vector2(1,0),true]]
+	Shapes[3] = [[Vector2(0,0),true],[Vector2(0,1),false],[Vector2(-1,1),true],[Vector2(0,2),true],[Vector2(1,1),true]]
+	Shapes[4] = [[Vector2(0,0),true],[Vector2(0,1),true]]
 	
 	
+	Shapes[5] = [[Vector2(0,0),true],[Vector2(0,1),false],[Vector2(0,2),false],[Vector2(0,3),true]]
 	
-func mirror_horizontal(arr: Array):
-	var result = []
+	Shapes[6] = [[Vector2(0,0),true],[Vector2(0,1),true],[Vector2(0,2),true],[Vector2(1,1),true]]
 	
-	for i in range(arr.size()):
-		var row = []
-		for j in range(arr[i].size()):
-			if arr[i][j] is Vector2:
-				var mirror_v = Vector2(arr[i][arr[i].size() - j - 1].x, arr[i][j].y)
-				row.append(mirror_v)
-			else:
-				row.append(arr[i][j])
-		result.append(row)
 	
-	return result
-
-
+	Shapes[7] = [[Vector2(0,0),true],[Vector2(0,1),true],[Vector2(0,2),true],[Vector2(1,1),true],[Vector2(0,3),true],[Vector2(1,2),true]]
 	
-
+	Shapes[8] = [[Vector2(0,0),true],[Vector2(0,1),false],[Vector2(1,1),false],[Vector2(1,2),true]]
+	
+	Shapes[9] = [[Vector2(0,0),true],[Vector2(0,1),false,'sink']]
+	Shapes[10]  = [[Vector2(0,0),false,'sink']]
+	# mirror_horizontal(Shapes[9])
+	Shapes[11] = [[Vector2(0,0),false],[Vector2(0,1),true],[Vector2(1,0),false],[Vector2(1,-1),true]]
+	Shapes[12] = [[Vector2(0,0),true],[Vector2(1,0),false],[Vector2(1,-1),false],[Vector2(1,-2),true]]
+	
+	Shapes[13] = [[Vector2(0,0),true],[Vector2(1,0),false],[Vector2(1,1),false],[Vector2(2,0),false],[Vector2(2,-1),true],[Vector2(3,0),true],]
+	
+	Shapes[14] = [[Vector2(0,0),'connector']]
+	
+	Shapes[15] = [[Vector2(0,0),'bomb']]
+	
+	
+	calculate_cost()
+	
+func calculate_cost():
+	for e in Shapes.size():
+		Cost[e] = len(Shapes[e])
